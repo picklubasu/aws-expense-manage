1a6ae68359b07c5560fe82d38ca181ad7f93be5f
Saptarsi Basu <basu.saptarsi91@gmail.com>
2023-08-25 23:11:48 +0530

    Build fix Payload fix

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -7,7 +7,7 @@ build_application() {
 package_deploy() {
 	#sam deploy --template-file ${CODEBUILD_SRC_DIR}/template.yml --stack-name llypharma-data-execution --parameter-overrides "$(jq -j 'to_entries[] | "\(.key)='\\\"'\(.value)'\\\"''\ '"' params.json)"
 	SAM_PARAMETERS=$( cat ${CODEBUILD_SRC_DIR}/payload.json | jq -r ' .Parameters | to_entries[] | "\(.key)='\\\"'\(.value)'\\\"''\ '"' )
-	sam deploy --template-file ${CODEBUILD_SRC_DIR}/template.json --stack-name "sns-email-topic-estimated-budget-notification" --parameter-overrides $SAM_PARAMETERS
+	sam deploy --template-file ${CODEBUILD_SRC_DIR}/template.json --stack-name "cft-stack-estimated-budget-notification" --parameter-overrides $SAM_PARAMETERS
 }
 
 echo "Starting build - $(date)"
